04188565f489b658a04b3cedec73d780c1ff0fca
baldwinIV <ksanghun10@gmail.com>
2021-09-05 03:16:12 +0900

    객체 보내도록 업데이트

--- exercise/filesender.js
+++ exercise/filesender.js
@@ -1,20 +1,36 @@
 var mqtt = require("mqtt");
-var server = mqtt.connect("mqtt://192.168.1.54:1883");
-var fs = require("fs");
-var a = 1;
-var tosend = new Object();
-tosend.data = a;
+var server = mqtt.connect("ws://test.mosquitto.org:8080");
+//연결하고 싶은 mqtt브로커 주소입니다. 
+var tosend = {
+  main: {
+    Hour1: 1.0,
+    Hour2: 2.0,
+    Hour3: 3.0,
+    Hour4: 4.0,
+    Hour5: 5.0,
+    Hour6: 6.0,
+    Hour7: 7.0,
+    Hour8: 8.0,
+    Hour9: 9.0,
+    Hour10: 10.0,
+    Hour11: 11.0,
+    Hour12: 12.0,
+  },
+  possible: 1,
+  type: "temp",
+};
+//tosend는 보내고 싶은 객체입니다. 
 var jsonData = JSON.stringify(tosend);
 server.on("connect", function () {
   console.log("sub on");
   server.publish("send", jsonData);
   var myTimer = setInterval(function () {
     var jsonData = JSON.stringify(tosend);
-    server.publish("send", jsonData);
-    console.log(tosend);
-    tosend.data = a;
-    a++;
-  }, 1);
+    server.publish("24hours_data_answer",jsonData);
+    //1패러미터의 topic으로 tosend를 문자열화 한 jsonData를 pub합니다.
+    console.log(jsonData);
+  }, 1000);
+  //1000은 1초입니다. 설정한 파라미터만큼 인터벌을 두고 퍼블리시합니다.
 
   //clearInterval(myTimer);
 });
